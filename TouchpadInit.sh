20c9b0b5b29274845de39752a3c904a413d60546
r888800009 <r888800009@gmail.com>
2018-04-21 23:17:22 +0800

    update TouchpadInit.sh

--- TouchpadInit.sh
+++ TouchpadInit.sh
@@ -4,4 +4,8 @@ source ~/.r809env
 #
 #r809_touchpad="Touchpad 1"
 #
-xinput set-prop "$r809_touchpad" 276 1 
+xinput set-prop "$r809_touchpad" "libinput Tapping Enabled" 1 
+xinput set-prop "$r809_touchpad" "libinput Natural Scrolling Enabled" 1   
+xinput set-prop "$r809_touchpad" "libinput Disable While Typing Enabled" 0 
+xinput set-prop "$r809_touchpad" "libinput Click Method Enabled"  {0 1\}
+xinput set-prop "$r809_touchpad" "libinput Accel Speed" 1 
