f4f38c741fb3626aa3a68ce0eee09e7c21eb7d2e
r888800009 <r888800009@gmail.com>
2018-04-25 04:02:06 +0800

    move to systemd timer

--- task-hooks/on-exit-sync.sh
+++ task-hooks/on-exit-sync.sh
@@ -7,16 +7,8 @@ do
     n=$(($n + 1))
 done
 
-timestamp_path=~/.task/r809hooksynctime.txt 
-source $timestamp_path 2> /dev/null  
-if [[ $? != 0 ]]; then
-    time1=0
-fi
 
-time2=$(date +%s)
-
-if [[ $(($time2-$time1)) -ge 300 ]] || (($n > 0)); then
-    echo "time1=$time2" > $timestamp_path
+if (($n > 0)); then   
     task sync &
 fi
 
