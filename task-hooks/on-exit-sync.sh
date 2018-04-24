0be6d620a127e8ac37f1e241aa5856cfe3b183ed
r888800009 <r888800009@gmail.com>
2018-04-25 03:16:17 +0800

    update sync sript

--- task-hooks/on-exit-sync.sh
+++ task-hooks/on-exit-sync.sh
@@ -7,8 +7,17 @@ do
     n=$(($n + 1))
 done
 
-if (($n > 0)); then
-    task sync
+timestamp_path=~/.task/r809hooksynctime.txt 
+source $timestamp_path 2> /dev/null  
+if [[ $? != 0 ]]; then
+    time1=0
+fi
+
+time2=$(date +%s)
+
+if [[ $(($time2-$time1)) -ge 300 ]] || (($n > 0)); then
+    echo "time1=$time2" > $timestamp_path
+    task sync &
 fi
 
 exit 0
